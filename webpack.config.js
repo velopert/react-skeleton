5949d0ee45c8be82c6b82580b517a2ec00b53835
velopert <public.velopert@gmail.com>
2016-05-24 22:24:12 +0900

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: __dirname,
+        path: __dirname + '/public',
         filename: 'bundle.js'
     },
 
     devServer: {
         inline: true,
-        port: 7777
+        port: 7777,
+        contentBase: __dirname + '/public'
     },
 
     module:
